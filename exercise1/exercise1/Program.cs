96ec585876dc2d69d1ef3bf8f24f0b4c0060b492
dohaisalive <dohaisalive@gmail.com>
2020-12-20 20:10:18 +0300

    the right file is here

---
+++ exercise1/exercise1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string charactername = "doha";
+            double speed = 1.65;
+            int age = 21;
+            string superpower1 = "telepathy";
+            string superpower2 = "mind reading";
+
+            Console.WriteLine("hello my name is {0}, and my superpowers are {1} and {2}. i am {3} years old and my speed is {4}. ", charactername, superpower1, superpower2, age, speed);
+        }
+    }
+}
