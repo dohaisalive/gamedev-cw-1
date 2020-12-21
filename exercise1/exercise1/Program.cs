88f1622ac52959933df023ee2dbd610cb42f0d60
dohaisalive <dohaisalive@gmail.com>
2020-12-21 17:33:21 +0300

    exercise 2

--- exercise1/exercise1/Program.cs
+++ exercise1/exercise1/Program.cs
@@ -13,6 +13,16 @@ namespace exercise1
             string superpower2 = "mind reading";
 
             Console.WriteLine("hello my name is {0}, and my superpowers are {1} and {2}. i am {3} years old and my speed is {4}. ", charactername, superpower1, superpower2, age, speed);
+
+
+            string heroname = "mario";
+            double heroheight = 1.14;
+            int heroage = 45;
+            string herosuperpower1 = "super jump";
+            string herosuperpower2 = "flying";
+
+            int agedifference = heroage - age;
+            Console.WriteLine("the age difference is {0}", agedifference);
         }
     }
 }
