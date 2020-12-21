b33f42074110ce5a2c15ad87814bcfb36f9552dc
dohaisalive <dohaisalive@gmail.com>
2020-12-21 18:38:00 +0300

    cw2

--- exercise1/exercise1/Program.cs
+++ exercise1/exercise1/Program.cs
@@ -3,9 +3,11 @@
 namespace exercise1
 {
     class Program
-    {
+    {       
+        
         static void Main(string[] args)
-        {
+        {   
+            //exercise 1
             string charactername = "doha";
             double speed = 1.65;
             int age = 21;
@@ -14,7 +16,7 @@ namespace exercise1
 
             //Console.WriteLine("hello my name is {0}, and my superpowers are {1} and {2}. i am {3} years old and my speed is {4}. ", charactername, superpower1, superpower2, age, speed);
 
-
+            //exercise 2
             string heroname = "mario";
             double heroheight = 1.14;
             int heroage = 45;
@@ -28,6 +30,12 @@ namespace exercise1
                 Console.WriteLine("the hero and the villan share a superpower!!");
             else
                 Console.WriteLine("the hero and the villan dont share any superpower");
+
+
+
+
         }
+
+       
     }
 }
